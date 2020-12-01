8bbb2794050357ff1d54748651b4e743f29362e7
Michael A. Cook <mcook0775@gmail.com>
2020-12-01 17:57:30 -0500

    Add padding to dashboard buttons

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -21,7 +21,7 @@ export default ({ user }) => {
 
       <Grid padded stackable style={{ marginTop: "50px" }}>
         <Grid.Column width={5}>
-          <Button fluid as={Link} to="/budgets/new" raised size="big">
+          <Button className="dashboard-btn" fluid as={Link} to="/budgets/new" raised size="big">
             <Icon
               size="large"
               name="add"
@@ -32,21 +32,21 @@ export default ({ user }) => {
         </Grid.Column>
 
         <Grid.Column width={5}>
-          <Button fluid as={Link} to="/budgets/manage" raised size="big">
+          <Button className="dashboard-btn" fluid as={Link} to="/budgets/manage" raised size="big">
             <Icon size="large" name="edit" style={{ color: "indianRed " }} />
             Manage Budgets
           </Button>
         </Grid.Column>
 
         <Grid.Column width={5}>
-          <Button fluid as={Link} to="/expenditures/new" raised size="big">
+          <Button className="dashboard-btn" fluid as={Link} to="/expenditures/new" raised size="big">
             <Icon size="large" name="dollar" style={{ color: "darkOrange " }} />
             Record Expenditures
           </Button>
         </Grid.Column>
 
         <Grid.Column width={5}>
-          <Button fluid as={Link} to="/finances/manage" raised size="big">
+          <Button className="dashboard-btn" fluid as={Link} to="/finances/manage" raised size="big">
             <Icon
               size="large"
               name="chart pie"
@@ -58,6 +58,7 @@ export default ({ user }) => {
 
         <Grid.Column width={5}>
           <Button
+            className="dashboard-btn"
             fluid
             as={Link}
             to="/expenditures/current/statistics"
@@ -74,14 +75,14 @@ export default ({ user }) => {
         </Grid.Column>
 
         <Grid.Column width={5}>
-          <Button fluid as={Link} to="/expeditures/current" raised size="big">
+          <Button className="dashboard-btn" fluid as={Link} to="/expeditures/current" raised size="big">
             <Icon size="large" name="money" style={{ color: "dodgerBlue " }} />
             View Spending
           </Button>
         </Grid.Column>
 
         <Grid.Column width={5}>
-          <Button fluid as={Link} to="/settings" raised size="big">
+          <Button className="dashboard-btn" fluid as={Link} to="/settings" raised size="big">
             <Icon size="large" name="setting" style={{ color: "salmon " }} />
             App Settings
           </Button>
