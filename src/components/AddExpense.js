f078b062ad521272770cb57d684fb837a9d8f7fd
Michael A. Cook <mcook0775@gmail.com>
2020-12-05 21:09:36 -0500

    add more features

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -5,6 +5,8 @@ import {
   Divider,
   Form,
   Header,
+  Icon,
+  List,
   Segment,
 } from "semantic-ui-react"
 import Breadcrumb from "./Breadcrumb"
@@ -31,7 +33,7 @@ export default () => {
   const [category, setCategory] = useState("")
   const [date, setDate] = useState(new Date())
   const [loading, setLoading] = useState(false)
-  const [payload, setPayload] = useState([])
+  const [payload, setPayload] = useState("")
   const [titles, setTitles] = useState([
     { key: 1, text: "Personal", value: "personal" },
     { key: 2, text: "Business", value: "business" },
@@ -48,28 +50,28 @@ export default () => {
   }
 
   /**
-   * Clear state variables needed after adding to payload
-   */
-  const clearState = () => {
-    setAmount("")
-    setCategory("")
-    setDate(new Date())
-  }
-
-  /**
    * Add a new expenditure to the payload
    */
   const addToPayload = () => {
     setPayload([
       ...payload,
       {
-        budget,
+        budget: budget.title,
         category,
         amount,
         date,
       },
     ])
-    clearState()
+    setAmount("")
+  }
+
+  /**
+   * Remove an expenditure from state
+   * @param {Number} i - expenditure index
+   */
+  const removeFromPayload = (i) => {
+    const newPayload = payload.filter((item, index) => index !== i)
+    setPayload(newPayload)
   }
 
   return (
@@ -83,8 +85,14 @@ export default () => {
       />
       <Segment raised className="mt-2" style={{ padding: "35px" }}>
         <Header as="h2">Add Expenses</Header>
-        <Form className="mt-1" fluid>
-          <Form.Group>
+        <Form className="mt-1">
+          <Form.Group widths="equal">
+            <Form.Input
+              type="date"
+              label="Date"
+              value={date}
+              onChange={(e, data) => setDate(data.value)}
+            />
             <Form.Select
               label="Budget"
               options={titles}
@@ -103,18 +111,47 @@ export default () => {
               value={amount}
               onChange={(e) => setAmount(e.target.value)}
             ></Form.Input>
-            <Form.Input
-              type="date"
-              label="Date"
-              value={date}
-              onChange={(e, data) => setDate(data.value)}
-            />
           </Form.Group>
-          <Button onClick={addToPayload}>Add</Button>
+          <Button color="green" size="tiny" onClick={addToPayload}>
+            <Icon name="plus" />
+            Add
+          </Button>
         </Form>
+        {payload ? (
+          <React.Fragment>
+            <List divided style={{ marginTop: "22px" }}>
+              {payload.map((item, i) => (
+                <List.Item key={i}>
+                  <List.Content floated="right">
+                    <Button
+                      onClick={() => removeFromPayload(i)}
+                      size="mini"
+                      color="red"
+                      float="right"
+                      icon="minus"
+                    ></Button>
+                  </List.Content>
+                  <List.Content>
+                    <List.Header>{item.budget}</List.Header>
+                    <List.Description>
+                      {item.category.toUpperCase()}, ${item.amount}
+                    </List.Description>
+                    <List.Description>{item.date}</List.Description>
+                  </List.Content>
+                </List.Item>
+              ))}
+            </List>
+          </React.Fragment>
+        ) : null}
         <Divider />
-        <Button loading={loading} onClick={(e) => setLoading(!loading)} primary>
-          Done
+        <Button
+          compact
+          size="big"
+          loading={loading}
+          onClick={(e) => setLoading(!loading)}
+        >
+          <Icon name="save" />
+          Save
         </Button>
       </Segment>
     </Container>
