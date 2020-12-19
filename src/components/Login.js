45d6bf848f7b13930b3ae766e05269fc5f16d50c
Michael A. Cook <mcook0775@gmail.com>
2020-12-19 17:34:27 -0500

    Changes to appearance, error message

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,11 +4,10 @@ import Cookies from "js-cookie"
 import {
   Button,
   Checkbox,
+  Container,
   Form,
   Header,
-  Grid,
   Message,
-  Segment,
 } from "semantic-ui-react"
 
 export default function Login({ login }) {
@@ -46,44 +45,41 @@ export default function Login({ login }) {
   }
 
   return (
-    <Grid centered doubling columns={3} className="mt-5">
-      <Grid.Column>
-        <Segment raised centered>
-          {queryParams.get("newsignup") ? (
-            <Message color="green">Awesome! You can login now</Message>
-          ) : null}
-          <Header as="h1" textAlign="center">
+    <Container>
+      {queryParams.get("newsignup") ? (
+        <Message color="green">Awesome! You can login now</Message>
+      ) : null}
+      <Header as="h1" className="mt-3">
+        Login
+      </Header>
+      <Form>
+        <Form.Field width={10}>
+          <input
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field width={10}>
+          <input
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+          />
+        </Form.Field>
+        <Form.Field>
+          <Form.Field width={10}>
+            <Checkbox
+              label="Keep me logged in"
+              onChange={(e) => setCheckbox(!checkbox)}
+            />
+            {error ? <Message color="red">{error.message}</Message> : null}
+          </Form.Field>
+
+          <Button onClick={doLogin} size="big" color="primary">
             Login
-          </Header>
-          <Form size="big">
-            <Form.Field>
-              <input
-                placeholder="username"
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </Form.Field>
-            <Form.Field>
-              <input
-                placeholder="password"
-                onChange={(e) => setPassword(e.target.value)}
-                type="password"
-              />
-            </Form.Field>
-            <Form.Field>
-              {error ? <p style={{ color: "red" }}>{error.message}</p> : null}
-              <Form.Field>
-                <Checkbox
-                  label="Keep me logged in"
-                  onChange={(e) => setCheckbox(!checkbox)}
-                />
-              </Form.Field>
-              <Button onClick={doLogin} size="big" color="primary" fluid>
-                Login
-              </Button>
-            </Form.Field>
-          </Form>
-        </Segment>
-      </Grid.Column>
-    </Grid>
+          </Button>
+        </Form.Field>
+      </Form>
+    </Container>
   )
 }
