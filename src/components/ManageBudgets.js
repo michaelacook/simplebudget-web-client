01600dc801378c5ff2848de037ab2632e4c7a083
Michael A. Cook <mcook0775@gmail.com>
2020-12-24 11:59:16 -0500

    Add message and link to create budget when user has no budgets

--- src/components/ManageBudgets.js
+++ src/components/ManageBudgets.js
@@ -18,31 +18,38 @@ export default ({ budgets, deleteBudget }) => {
       <Segment raised className="mt-2" style={{ padding: "35px" }}>
         <Header as="h2">Manage Budgets</Header>
         <List divided relaxed className="mt-2">
-          {budgets
-            ? budgets.map((budget) => (
-                <List.Item style={{ marginBottom: "12px" }}>
-                  <List.Content>
-                    <List.Content floated="right">
-                      <Button
-                        as={Link}
-                        to={`/budgets/manage/${budget.id}/`}
-                        compact
-                        icon="edit"
-                        color="green"
-                      />
-                      <ConfirmDeleteBudgetModal
-                        deleteBudget={deleteBudget}
-                        budgetId={budget.id}
-                      />
-                    </List.Content>
-                    <List.Header as={Link} to={`/budgets/${budget.id}`}>
-                      {budget.title}
-                    </List.Header>
-                    <List.Description>{budget.description}</List.Description>
+          {budgets ? (
+            budgets.map((budget) => (
+              <List.Item style={{ marginBottom: "12px" }}>
+                <List.Content>
+                  <List.Content floated="right">
+                    <Button
+                      as={Link}
+                      to={`/budgets/manage/${budget.id}/`}
+                      compact
+                      icon="edit"
+                      color="green"
+                    />
+                    <ConfirmDeleteBudgetModal
+                      deleteBudget={deleteBudget}
+                      budgetId={budget.id}
+                    />
                   </List.Content>
-                </List.Item>
-              ))
-            : null}
+                  <List.Header as={Link} to={`/budgets/${budget.id}`}>
+                    {budget.title}
+                  </List.Header>
+                  <List.Description>{budget.description}</List.Description>
+                </List.Content>
+              </List.Item>
+            ))
+          ) : (
+            <React.Fragment>
+              <Header as="h5">You have not yet created any budgets.</Header>
+              <Button as={Link} to="/budgets/new">
+                Create New Budget
+              </Button>
+            </React.Fragment>
+          )}
         </List>
       </Segment>
     </Container>
